3c24f48bd5d515449dcafb2e7459fa4a0350939e
Ashwin sivasankar v <109450234+Ashwin20BCE1804@users.noreply.github.com>
2023-06-07 18:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+let display = document.getElementById("display");
+let countDown = 10;
+let message = "Happy Independence Day";
+setTimeout(() => {
+  display.innerText = countDown;
+  countDown--;
+  setTimeout(() => {
+    display.innerText = countDown;
+    countDown--;
+    setTimeout(() => {
+      display.innerText = countDown;
+      countDown--;
+      setTimeout(() => {
+        display.innerText = countDown;
+        countDown--;
+        setTimeout(() => {
+          display.innerText = countDown;
+          countDown--;
+          setTimeout(() => {
+            display.innerText = countDown;
+            countDown--;
+            setTimeout(() => {
+              display.innerText = countDown;
+              countDown--;
+              setTimeout(() => {
+                display.innerText = countDown;
+                countDown--;
+                setTimeout(() => {
+                  display.innerText = countDown;
+                  countDown--;
+                  setTimeout(() => {
+                    display.innerText = countDown;
+                    countDown--;
+                    setTimeout(() => {
+                      display.innerText = message;
+                      if (display.innerText == "Happy Independence Day") {
+                        display.classList.add("zoom");
+                      }
+                    }, 3000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
